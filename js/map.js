182d0d5bf168b7a3f2402e121d14dc92910a9747
yekaiyu <yekaiyu94@gmail.com>
2020-07-31 01:30:37 -0500

    add a zoom...

--- js/map.js
+++ js/map.js
@@ -5,7 +5,7 @@ const dailyTotal = d3.json('../data/dailyTotal.json');
 
 const width = 1000;
 const height = 600;
-const svg = d3.select("svg")
+const svg = d3.select("#map")
               .attr("width", width)
               .attr("height", height);
 
@@ -46,8 +46,6 @@ var tooltip = d3.select("body")
 
 var logScale = d3.scaleLog().domain([0.1,20695922]).range([1,13]);
 
-
-
 Promise.all([us, covid, stateCodeMapping, dailyTotal])
   .then(function (values) {
     var map = values[0];
@@ -82,6 +80,11 @@ Promise.all([us, covid, stateCodeMapping, dailyTotal])
             d3.select(this)
                 .style('fill-opacity', .7);
             tooltip.style("visibility", "hidden");
+        })
+        .on('click', function(d, i) {
+          var sliderInput = document.getElementById("currSelectedDate");
+          var dateSelection = allDate[sliderInput.value];
+          drawOneStateChart(mapping[d.id], dateSelection);
         });
     // circle
     svg.append("g").attr("pointer-events", "none").selectAll("circle")
@@ -140,6 +143,18 @@ function drawCircles() {
 };
 
 
+/*
+  plot each county of a selected state on a given date
+*/
+function drawOneStateChart(stateName, date) {
+  console.log(stateName);
+  console.log(date);
+  var svg = d3.select("#oneStateChart")
+                .attr("width", width)
+                .attr("height", height);
+
+}
+
 function drawStateChart(dailyData) {
   /*
   * line chart
@@ -148,46 +163,134 @@ function drawStateChart(dailyData) {
   var margin = ({top: 20, right: 30, bottom: 30, left: 40});
 
   var stateChartSVG = d3.select("#stateChart")
-                            .attr("width", width)
-                            .attr("height", height);
+                          .attr("width", width)
+                          .attr("height", height);
   var line = d3.line()
-                .x(function(d) { return xScale(new Date(d.date)); })
+                .x(function(d) { /*console.log(d);*/ return xScale(new Date(d.date)); })
                 .y(function(d) { return yScale(d.cases); });
   var xScale = d3.scaleTime()
-              .domain([startDate, endDate])
-              .range([margin.left, width - margin.right]);
+                  .domain([startDate, endDate])
+                  .range([margin.left, width - margin.right]);
+  // var xScale = d3.scaleOrdinal()
+  //                 .domain(allDate)
+  //                 .range([margin.left, width - margin.right]);
   var yScale = d3.scaleLog()
-              .domain([1, dailyData[dailyData.length - 1].cases]).nice()
-              .range([height - margin.bottom, margin.top]);
-  var xAxis = g => g
-      .attr("transform", `translate(0,${height - margin.bottom})`)
-      .call(d3.axisBottom(xScale));
-  var yAxis = g => g
-      .attr("transform", `translate(${margin.left},0)`)
-      .call(d3.axisLeft(yScale).ticks(10, "~s"));
+                  .domain([1, dailyData[dailyData.length - 1].cases]).nice()
+                  .range([height - margin.bottom, margin.top]);
+  var xAxis = stateChartSVG.append("g")
+                            .attr("class", "stateX")
+                            .attr("transform", `translate(0,${height - margin.bottom})`)
+                            .call(d3.axisBottom(xScale));
+  // var xAxis = d3.axisBottom(xScale);
+  var yAxis = stateChartSVG.append("g")
+                            .attr("class", "stateY")
+                            .attr("transform", `translate(${margin.left},0)`)
+                            .call(d3.axisLeft(yScale).ticks(10, "~s"));
+  // var yAxis = d3.axisLeft(yScale).ticks(10, "~s");
 
   function transition(path) {
           path.transition()
               .duration(3000)
               .attrTween("stroke-dasharray", tweenDash);
-  }
+  };
+
   function tweenDash() {
-  var l = this.getTotalLength(),
-      i = d3.interpolateString("0," + l, l + "," + l);
-  return function (t) { return i(t); };
+    var l = this.getTotalLength(),
+        i = d3.interpolateString("0," + l, l + "," + l);
+    return function (t) {
+      return i(t);
+    };
+  };
+
+  stateChartSVG.append("path")
+                .datum(dailyData)
+                .attr("class", "stateLine")
+                .attr("fill", "none")
+                .attr("stroke", "steelblue")
+                .attr("stroke-width", 1.5)
+                .attr("d", line)
+                .call(transition);
+
+// zoom
+  stateChartSVG.call(zoom);
+
+  function zoom(svg) {
+    var extent = [
+			[margin.left, margin.top],
+			[width - margin.right, height - margin.top]
+		];
+
+		var zooming = d3.zoom()
+			.scaleExtent([1, 3])
+			.translateExtent(extent)
+			.extent(extent)
+			.on("zoom", zoomed);
+
+		svg.call(zooming);
+
+		function zoomed() {
+
+			xScale.range([margin.left, width - margin.right]
+				.map(d => d3.event.transform.applyX(d)));
+
+			svg.select(".stateLine")
+				.attr("d", line);
+
+			svg.select(".stateX")
+				.call(d3.axisBottom(xScale)
+					.tickSizeOuter(0));
+		}
+	}
+
+// brush to zoom-in
+  // var brush = d3.brushX()
+  //                 .extent([[0, 0], [width - margin.right, height - margin.bottom]])
+  //                 .on("end", updateChart);
+  // stateChartSVG.append("g")
+  //               .attr("class", "brush")
+  //               .call(brush);
+
+  var idleTimeout;
+  function idled() {
+    idleTimeout = null;
+  };
+
+  function updateChart() {
+    extent = d3.event.selection;
+    var xSelectLeft;
+    var xSelectRight;
+    // console.log(extent);
+    // console.log(extent ? xScale.invert(extent[0]) : NaN);
+    // console.log(extent ? xScale.invert(extent[1]) : NaN);
+    if(!extent) {
+      if(!idleTimeout) {
+        return idleTimeout = setTimeout(idled, 350);
+      }
+        //
+    } else {
+      xSelectLeft = xScale.invert(extent[0]);
+      xSelectRight = xScale.invert(extent[1]);
+      // console.log(xSelectLeft);
+      // console.log(xSelectRight);
+      xScale.domain([xScale.invert(extent[0]), xScale.invert(extent[1])]);
+      stateChartSVG.select(".brush")
+                    .call(brush.move, null);
+    }
+
+    xAxis.transition().duration(1000)
+          .call(d3.axisBottom(xScale));
+
+    stateChartSVG.select(".stateLine")
+                  .transition()
+                  .duration(1000)
+                  .attr("d", line);
   }
-  // function drawStateChart() {
-    stateChartSVG.append("g")
-                    .attr("class", "stateX")
-                    .call(xAxis);
-    stateChartSVG.append("g")
-                    .attr("class", "stateY")
-                    .call(yAxis);
-    stateChartSVG.append("path")
-                  .datum(dailyData)
-                  .attr("fill", "none")
-                  .attr("stroke", "steelblue")
-                  .attr("stroke-width", 1.5)
-                  .attr("d", line)
-                  .call(transition);
+
+  stateChartSVG.on("dblclick", function() {
+    xScale.domain([startDate, endDate]);
+    xAxis.transition().call(d3.axisBottom(xScale));
+    stateChartSVG.select(".stateLine")
+                  .transition()
+                  .attr("d", line);
+  });
 }
